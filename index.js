3b9336ce6c4ff2e330e24b1380ec6b806a4358a9
kssandhu1 <sandhuka@oregonstate.edu>
2022-12-29 12:56:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 // 'npm run dev' to start server using nodemon!!!
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
