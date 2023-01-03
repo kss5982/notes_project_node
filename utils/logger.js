c822e6981e33c71562e5364e9ed4741515bb2d3b
kssandhu1 <sandhuka@oregonstate.edu>
2023-01-03 02:48:47 -0500

    testing

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,9 @@
+// info will print all normal log messages
 const info = (...params) => {
   console.log(...params);
 };
 
+// error will print for all error messages
 const error = (...params) => {
   console.error(...params);
 };
