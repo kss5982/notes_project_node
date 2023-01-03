90d4a0cf5ddf6718bc3eca7004ffdaf157944463
kssandhu1 <sandhuka@oregonstate.edu>
2023-01-03 03:24:09 -0500

    testing

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,15 @@
 // info will print all normal log messages
 const info = (...params) => {
-  console.log(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
 };
 
 // error will print for all error messages
 const error = (...params) => {
-  console.error(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
 };
 
 module.exports = {
