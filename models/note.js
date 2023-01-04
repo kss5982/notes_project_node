dfd3649c591ffbe9cd92bd70c7579e9fed0461d8
kssandhu1 <sandhuka@oregonstate.edu>
2023-01-04 00:35:38 -0500

    Update note.js

--- models/note.js
+++ models/note.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
-  date: {
-    type: Date,
-    required: true,
-  },
+  date: Date,
   important: Boolean,
 });
 
