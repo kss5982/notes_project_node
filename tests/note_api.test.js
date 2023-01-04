08666e95242bfa88a99f548f7f45aab724354d37
kssandhu1 <sandhuka@oregonstate.edu>
2023-01-04 00:33:23 -0500

    Updated testing stuff

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,17 +1,16 @@
-const supertest = require("supertest");
 const mongoose = require("mongoose");
-const helper = require("./test_helper");
+const supertest = require("supertest");
 const app = require("../app");
+
 const api = supertest(app);
 
 const Note = require("../models/note");
+const helper = require("./test_helper");
 
 beforeEach(async () => {
   await Note.deleteMany({});
-
   let noteObject = new Note(helper.initialNotes[0]);
   await noteObject.save();
-
   noteObject = new Note(helper.initialNotes[1]);
   await noteObject.save();
 });
@@ -66,6 +65,36 @@ test("note without content is not added", async () => {
   const notesAtEnd = await helper.notesInDb();
 
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+
+test("a specific note can be viewed", async () => {
+  const notesAtStart = await helper.notesInDb();
+
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+
+  expect(resultNote.body).toEqual(processedNoteToView);
+});
+
+test("a note can be deleted", async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map((r) => r.content);
+
+  expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(() => {
