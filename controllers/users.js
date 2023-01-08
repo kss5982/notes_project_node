1c290c3425d7ffa3f5cad6df4aa023764db9b6af
kssandhu1 <sandhuka@oregonstate.edu>
2023-01-08 03:56:11 -0500

    Authorization

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,20 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
+  const users = await User.find({}).populate("notes", { content: 1, date: 1 });
 
-  response.json(notes);
+  response.json(users);
 });
 
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: "username must be unique",
+    });
+  }
 
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
